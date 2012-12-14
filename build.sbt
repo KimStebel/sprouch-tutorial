8a2f25222dac716c21018751e681dc1ed2c353cf
Kim Stebel <mail@kim-stebel.com>
2012-12-14 23:49:17 +0100

    create, update single document

--- build.sbt
+++ build.sbt
@@ -2,7 +2,7 @@ name := "sprouch-tutorial"
 
 scalaVersion := "2.9.2"
 
-libraryDependencies += "sprouch" % "sprouch_2.9.1" % "0.5"
+libraryDependencies += "sprouch" % "sprouch_2.9.1" % "0.5.1"
 
 resolvers += "sprouch repo" at "http://kimstebel.github.com/sprouch/repository"
 
--- src/main/scala/sprouch/tutorial/Main.scala
+++ src/main/scala/sprouch/tutorial/Main.scala
@@ -1,9 +1,29 @@
 package sprouch.tutorial
 
 import sprouch._
+import JsonProtocol._
+import akka.actor.ActorSystem
+import akka.util.Duration
+import akka.dispatch.Await
 
 object Main {
+  case class Contact(name:String, email:Option[String], phone:Option[String])
+  implicit val contactFormat = jsonFormat3(Contact)
+  
   def main(args:Array[String]) {
-    println("welcome to the sprouch tutorial.")
+    val actorSystem = ActorSystem("myActorSystem")
+    val duration = Duration("10 seconds")
+    val couch = Couch(Config(actorSystem))
+    val future = for {
+      db <- couch.createDb("testdb")
+      doc <- db.createDoc(Contact("john", Some("john@acme.org"), None))
+      updatedDoc <- db.updateDoc(doc.updateData(_.copy(phone = Some("12345"))))
+      _ <- db.delete()
+    } yield {
+      println("first version: " + doc)
+      println("second version: " + updatedDoc)
+    }
+    Await.result(future, duration)
+    actorSystem.shutdown()
   }
 }
